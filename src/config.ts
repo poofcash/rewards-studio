0da0ffafae9bb1ae30042582f70f723b79c7d83c
Brian Li <brnli7@gmail.com>
2021-07-27 14:09:17 -0700

    List tptCELO

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,5 @@ export const REGISTRY = [
   { address: "0x5d4E32975Bcf3aca47282C580Cf5Bc0F8135D218", symbol: "vcapCELO" },
   { address: "0xB05e342117f7cCE1646e0016804bCaA09741c593", symbol: "uCELO" },
   { address: "0x2879BFD5e7c4EF331384E908aaA3Bd3014b703fA", symbol: "sCELO" },
+  { address: "0x5AfDb9c5A20EE5bB36d9c81BD5E1BE56FBF52CD8", symbol: "tptCELO" },
 ];
